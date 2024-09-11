1b15fd6d35e6036db4b40ace2d8d3e5631ef50e8
Matheus297 <matheusabreu297@gmail.com>
2024-09-10 22:09:22 -0300

    Criando Navbar

---
+++ src/app/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const Navbar = styled.nav`
+    background-color: #111111;
+    height: 110px;
+    border-bottom-color: rgba(255,255,255,0.03);
+    border-bottom-width: 1;
+    border-bottom-style: solid;
+    padding: 0;
+`;
+
+
+export const Logo = styled.img`
+    color: #fff;
+    object-fit: contain;
+    max-width: 100%;
+    width: 200px;
+`;
